d0e092f2eb39a718109a1486211d1b1f47f63368
Miguel Martinez <slverwolf@gmail.com>
2018-08-16 20:54:22 -0500

    runtime_libart: disable minidebuginfo

--- target/product/runtime_libart.mk
+++ target/product/runtime_libart.mk
@@ -96,4 +96,4 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 
 # Enable minidebuginfo generation unless overridden.
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    dalvik.vm.dex2oat-minidebuginfo=true
+    dalvik.vm.dex2oat-minidebuginfo=false
